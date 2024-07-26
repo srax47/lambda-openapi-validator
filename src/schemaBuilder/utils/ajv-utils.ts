0292815820fa79c616a31a30286ff1633eb0bc27
Rami Jarrar <srax47@gmail.com>
2024-07-26 04:23:36 +0300

    chore: cleanup deps, tsconfig

--- src/schemaBuilder/utils/ajv-utils.ts
+++ src/schemaBuilder/utils/ajv-utils.ts
@@ -1,13 +1,15 @@
 import type Ajv from 'ajv'
 import type { format, keyword } from '../../types'
-import addFormats from 'ajv-formats'
+
+// uuid: http://tools.ietf.org/html/rfc4122
+const uuidRegix = /^(?:urn:uuid:)?[0-9a-f]{8}-(?:[0-9a-f]{4}-){3}[0-9a-f]{12}$/i
 
 export function addCustomKeyword(
   ajv: Ajv,
   formats?: format[],
   keywords?: keyword[],
 ) {
-  addFormats(ajv, ['uuid'])
+  ajv.addFormat('uuid', uuidRegix)
 
   formats?.forEach(function (format) {
     ajv.addFormat(format.name, format.pattern)
